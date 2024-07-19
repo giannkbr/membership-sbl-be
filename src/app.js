06163f993b53be166e1bf34ea6fd82a1260858b2
giannkbr <akbarigi20@gmail.com>
2024-07-19 10:27:44 +0700

    refactor : add cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const memberRoutes = require('./routes/memberRoutes');
@@ -5,6 +6,7 @@ require('dotenv').config();
 const sequelize = require('./config/database');
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/api', memberRoutes);
--- src/models/member.js
+++ src/models/member.js
@@ -4,7 +4,15 @@ const sequelize = require('../config/database');
 const Member = sequelize.define('Member', {
   fullName: { type: DataTypes.STRING, allowNull: false },
   placeOfBirth: { type: DataTypes.STRING, allowNull: false },
-  dateOfBirth: { type: DataTypes.DATE, allowNull: false },
+  dateOfBirth: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    validate: {
+      isDate: {
+        msg: 'Date of birth must be a valid date',
+      },
+    },
+  },
   phoneNumber: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, validate: { isEmail: true } },
   address: { type: DataTypes.TEXT, allowNull: false },
